0e34be0ca2b1ee1086fb68c366f65f64e5f807f9
Zhiyuan Liu <51340403+Liu-ZY-98@users.noreply.github.com>
2023-08-09 20:47:49 -0700

    Video comparison slider prototype working

--- js/main.js
+++ js/main.js
@@ -3,10 +3,23 @@ const beforeImage = document.querySelector(".image-comparison .before-image");
 const sliderLine = document.querySelector(".image-comparison .slider-line");
 const sliderIcon = document.querySelector(".image-comparison .slider-icon");
 
+const vid_slider = document.querySelector(".vid-comparison .vslider");
+const beforeVid = document.querySelector(".vid-comparison .before-vid");
+const vid_sliderLine = document.querySelector(".vid-comparison .vslider-line");
+const vid_sliderIcon = document.querySelector(".vid-comparison .vslider-icon");
+
 slider.addEventListener("input", (e) => {
     let sliderValue = e.target.value + "%";
 
     beforeImage.style.width = sliderValue;
     sliderLine.style.left = sliderValue;
     sliderIcon.style.left = sliderValue;
+});
+
+vid_slider.addEventListener("input", (e) => {
+    let sliderValue = e.target.value + "%";
+
+    beforeVid.style.width = sliderValue;
+    vid_sliderLine.style.left = sliderValue;
+    vid_sliderIcon.style.left = sliderValue;
 });
